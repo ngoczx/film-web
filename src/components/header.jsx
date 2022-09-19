1b8895c37a70844d90ea3a2dc479ced59faca9b8
ngoczx <ngocnguyen0323@gmail.com>
2022-09-20 00:02:10 +0700

    upload sourcecode

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -32,7 +32,7 @@ const Header = ({ trending }) => {
           spaceBetween={30}
           centeredSlides={true}
           autoplay={{
-            delay: 250000,
+            delay: 2500,
             disableOnInteraction: false,
           }}
           navigation={true}
